c736d65051aebaa9f6a131f28adcf416cb2291e1
Radmir Gurkov <ragux0@gmail.com>
2024-09-29 19:34:10 +0200

    try config update

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@ import createMDX from "@next/mdx";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   images: {
