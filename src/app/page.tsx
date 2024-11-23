56c0f68512bddf0dfd4717e3dcd1c7621c8ebd59
Radmir Gurkov <ragux0@gmail.com>
2024-11-23 21:25:08 +0100

    Workarond with use

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,47 @@
-import { getBlogPostList } from "@/utilts/fileUtils";
+import { getBlogPostList, Post } from "@/utilts/fileUtils";
 import Link from "next/link";
+import { Suspense, use, useMemo } from "react";
 
-export default async function Home() {
-  const posts = await getBlogPostList();
-  // dummy static issue in github actions build
-  const postsToUse = Array.isArray(posts) ? posts : [];
+type PostsListProps = {
+  postsPromise: Promise<Post[]>;
+};
 
-  const postsByLastModified = postsToUse.toSorted((a, b) => {
+const PostsList = ({ postsPromise }: PostsListProps) => {
+  const posts = use(postsPromise);
+
+  const postsByLastModified = posts.toSorted((a, b) => {
     return b.modifiedAt.getTime() - a.modifiedAt.getTime();
   });
 
   return (
+    <ul className="flex flex-col gap-y-2 mt-3">
+      {postsByLastModified.map((post) => {
+        return (
+          <Link
+            href={post.path}
+            className="bg-white h-full text-black p-4 rounded-sm"
+            key={post.path}
+          >
+            <p>{post.title}</p>
+            <p>{`Last modified at ${post.createdAt.toLocaleDateString()}`}</p>
+          </Link>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default async function Home() {
+  const postsPromise = useMemo(() => {
+    return getBlogPostList();
+  }, []);
+
+  return (
     <section>
       <h1 className="text-lg font-bold">Recent posts:</h1>
-      <ul className="flex flex-col gap-y-2 mt-3">
-        {postsByLastModified.map((post) => {
-          return (
-            <Link
-              href={post.path}
-              className="bg-white h-full text-black p-4 rounded-sm"
-              key={post.path}
-            >
-              <p>{post.title}</p>
-              <p>{`Last modified at ${post.createdAt.toLocaleDateString()}`}</p>
-            </Link>
-          );
-        })}
-      </ul>
+      <Suspense fallback={<p>Loading....</p>}>
+        <PostsList postsPromise={postsPromise} />
+      </Suspense>
     </section>
   );
 }
