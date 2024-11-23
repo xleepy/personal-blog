72cd57494e8e1f2ba47f7df97f91232aeeeea389
Radmir Gurkov <ragux0@gmail.com>
2024-11-23 21:34:38 +0100

    Fix sorting issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,31 @@
 import { getBlogPostList } from "@/utilts/fileUtils";
-import { PostsList } from "./PostsList";
+import Link from "next/link";
 
 export default async function Home() {
   const posts = await getBlogPostList();
 
+  // toSorted fails in github actions. switched to old approach
+  const postsByLastModified = posts.slice().sort((a, b) => {
+    return b.modifiedAt.getTime() - a.modifiedAt.getTime();
+  });
+
   return (
     <section>
       <h1 className="text-lg font-bold">Recent posts:</h1>
-      <PostsList posts={posts} />
+      <ul className="flex flex-col gap-y-2 mt-3">
+        {postsByLastModified.map((post) => {
+          return (
+            <Link
+              href={post.path}
+              className="bg-white h-full text-black p-4 rounded-sm"
+              key={post.path}
+            >
+              <p>{post.title}</p>
+              <p>{`Last modified at ${post.createdAt.toLocaleDateString()}`}</p>
+            </Link>
+          );
+        })}
+      </ul>
     </section>
   );
 }
