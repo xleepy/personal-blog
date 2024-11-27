d03031789e75f8469d862a2d00b2375872caabc8
xleepy <xleepy@users.noreply.github.com>
2024-11-27 20:27:45 +0000

    deploy: 110db6d44b3d80f22dd41ffd8683f3e28f60e377

---
+++ _next/static/chunks/app/about/page-730338f113f3c020.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[220,974],{2987:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,4839,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[839,441,517,358],()=>s(2987)),_N_E=e.O()}]);
